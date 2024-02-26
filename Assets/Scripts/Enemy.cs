2b966d33d6ab98cf4ee8ffb67b576b34ded9cf05
JonasKucinskas <jonaskucinskas2@gmail.com>
2024-02-26 16:24:15 +0200

    PRZ-21 enemy system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+	public int hp;
+	public int damage;
+	public float x;
+	public float y;
+}
