184052dbce1c053aa2fd60847c68e9800cc35f89
SunflowerBee <126417431+SunflowerBee@users.noreply.github.com>
2024-02-29 00:47:45 +0200

    Made cards bigger and changed their position

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -23,6 +23,26 @@ public class CardManager : MonoBehaviour
                     RandomCard.gameObject.SetActive(true);
                     RandomCard.transform.position=cardSlots[i].position;
                     RandomCard.SetSlotCoordinates(cardSlots[i].position);
+
+                    if (i == 0)
+                    {
+                        RandomCard.transform.Rotate(0f, 0f, 45f, Space.Self);
+                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.down * 0.2f);
+                    }
+                    else if (i == 1)
+                        RandomCard.transform.Rotate(0f, 0f, 20f, Space.Self);
+                    else if (i == 2)
+                    {
+                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.up * 0.065f);
+                    }
+                    else if (i == 3)
+                        RandomCard.transform.Rotate(0f, 0f, -20f, Space.Self);
+                    else if (i == 4)
+                    {
+                        RandomCard.transform.Rotate(0f, 0f, -45f, Space.Self);
+                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.down * 0.2f);
+                    }
+
                     freeCardSlots[i]=false;
                     deck.Remove(RandomCard);
                     return;
