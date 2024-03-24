c85281c2e9a98a81da306c32d191131bd7c9361e
JonasKucinskas <jonaskucinskas2@gmail.com>
2024-03-24 14:40:52 +0200

    modular card system

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -24,15 +24,22 @@ public class Card : MonoBehaviour
 	public GameObject CardModel;
 	public GameObject Particle;
     private TurnManager turnManager;
+	private bool hasMovedAboveThreshold = false;
 
-	private void Start()
+	public delegate void CardMovedFromHandEventHandler(Card card);
+    public event CardMovedFromHandEventHandler OnCardMovedFromHand;
+
+	public delegate void CardMovedToHandEventHandler(Card card);
+    public event CardMovedToHandEventHandler OnCardMovedToHand;
+
+
+	void Awake()
 	{
 		turnManager = GameObject.Find("TurnManager").GetComponent<TurnManager>();
 		objectScreenCoord = Camera.main.WorldToScreenPoint(transform.position);
-		originalPosition = transform.position;
 	}
 
-	private void Update()
+	void Update()
 	{
 		if (IsDragging)
 		{
@@ -66,8 +73,10 @@ public class Card : MonoBehaviour
 			// Factor in cumulative translations to get the correct original position
 			originalPosition = transform.position - cumulativeTranslation;
 
+			originalPosition = transform.position;
 			originalScale = transform.localScale;
 			originalRotation = transform.rotation;
+
 
 			objectScreenCoord = Camera.main.WorldToScreenPoint(transform.position);
 			offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, objectScreenCoord.z));
@@ -97,6 +106,7 @@ public class Card : MonoBehaviour
 	{
 		if (IsDragging)
 		{
+			hasMovedAboveThreshold = false;
 			IsDragging = false;
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
@@ -124,7 +134,8 @@ public class Card : MonoBehaviour
 	{
 		// Adjust original position to factor in cumulative translations
 		transform.position = originalPosition + cumulativeTranslation;
-
+		OnCardMovedToHand?.Invoke(this);
+		Debug.Log("RETURN INVOKED");
 		// Reset transformations
 		transform.localScale = originalScale;
 		transform.rotation = originalRotation;
@@ -160,6 +171,13 @@ public class Card : MonoBehaviour
 
 		Quaternion targetRotation = Quaternion.Euler(1, originalRotation.eulerAngles.y, 1);
 		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5);
+
+
+		if (transform.position.y >= originalPosition.y / 2 && !hasMovedAboveThreshold)
+		{
+			OnCardMovedFromHand?.Invoke(this);
+			hasMovedAboveThreshold = true;
+		}       
 	}
 
 	private void AttemptToPlaceCard()
@@ -192,47 +210,45 @@ public class Card : MonoBehaviour
 	{
 		TileScript tile = cubeTransform.GetComponent<TileScript>();
 
-		if (!tile.IsOccupied())
-		{
-			transform.position = cubeTransform.position + new Vector3(0.01f, 1f, 0);
-			transform.localScale *= 2f;
-			transform.SetParent(cubeTransform, worldPositionStays: true);
-			IsDragging = false;
-			isPlaced = true;
-
-			var dragObject = GetComponent<DragObject>();
-			if (dragObject != null)
-			{
-				dragObject.enabled = false;
-			}
-
-			// Optionally, deactivate the card and spawn a character model or effect
-			this.gameObject.SetActive(false);
-			GameObject model = CardModel;
-			Vector3 modelPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 0.3f, this.gameObject.transform.position.z);
-
-			// Instantiate particle effect and character model
-            Instantiate(Particle, modelPosition, Quaternion.Euler(0f, 90f, 0f), cubeTransform);
-            GameObject character = Instantiate(model.gameObject, modelPosition, Quaternion.Euler(0f, 90f, 0f), cubeTransform);
-            
-            DendriticCell friendly = character.AddComponent<DendriticCell>();
-            friendly.characterName = $"Friendly_{tile.xPosition}_{tile.zPosition}";
-            friendly.xPosition = tile.xPosition;
-            friendly.zPosition = tile.zPosition;
-
-            tile.SetFriendlyPresence(true);
-
-			turnManager.SubtractPlayerMove();
-			
-			Debug.Log("Card placed on cube.");
-		}
-		else
+		if (tile.IsOccupied())
 		{
 			Debug.Log("Cannot place card here, slot is already occupied or has an enemy.");
 
 			// Optionally, return the card to its original position or handle as desired
 			transform.position = originalPosition;
+			return;
 		}
+
+		transform.position = cubeTransform.position + new Vector3(0.01f, 1f, 0);
+		transform.localScale *= 2f;
+		transform.SetParent(cubeTransform, worldPositionStays: true);
+		IsDragging = false;
+		isPlaced = true;
+
+		var dragObject = GetComponent<DragObject>();
+
+		if (dragObject != null)
+		{
+			dragObject.enabled = false;
+		}
+
+		// Optionally, deactivate the card and spawn a character model or effect
+		this.gameObject.SetActive(false);
+		GameObject model = CardModel;
+		Vector3 modelPosition = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y - 0.3f, this.gameObject.transform.position.z);
+		
+		// Instantiate particle effect and character model
+        Instantiate(Particle, modelPosition, Quaternion.Euler(0f, 90f, 0f), cubeTransform);
+        GameObject character = Instantiate(model.gameObject, modelPosition, Quaternion.Euler(0f, 90f, 0f), cubeTransform);
+        
+        DendriticCell friendly = character.AddComponent<DendriticCell>();
+        friendly.characterName = $"Friendly_{tile.xPosition}_{tile.zPosition}";
+        friendly.xPosition = tile.xPosition;
+        friendly.zPosition = tile.zPosition;
+        tile.SetFriendlyPresence(true);
+		turnManager.SubtractPlayerMove();
+		
+		Debug.Log("Card placed on cube.");
 	}
 	
 	public void SetSlotCoordinates(Vector3 slot)
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,67 +1,114 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
     public GameObject MovesLeftText;
-    public List<Card> deck =new List<Card>();
-    public Transform[] cardSlots;
-    public bool[] freeCardSlots;
-
-    public void DrawCard()
-    {
-        if(deck.Count >= 1)
-        {
-            Card RandomCard = deck[Random.Range(0,deck.Count)];
-            
-            for(int i = 0; i<freeCardSlots.Length; i++)
-            {
-                if(freeCardSlots[i]==true)
-                {
-                    RandomCard.gameObject.SetActive(true);
-                    RandomCard.transform.position=cardSlots[i].position;
-                    RandomCard.SetSlotCoordinates(cardSlots[i].position);
-
-                    if (i == 0)
-                    {
-                        RandomCard.transform.Rotate(0f, 0f, 45f, Space.Self);
-                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.down * 0.2f);
-                    }
-                    else if (i == 1)
-                        RandomCard.transform.Rotate(0f, 0f, 20f, Space.Self);
-                    else if (i == 2)
-                    {
-                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.up * 0.065f);
-                    }
-                    else if (i == 3)
-                        RandomCard.transform.Rotate(0f, 0f, -20f, Space.Self);
-                    else if (i == 4)
-                    {
-                        RandomCard.transform.Rotate(0f, 0f, -45f, Space.Self);
-                        RandomCard.transform.position += RandomCard.transform.TransformDirection(Vector3.down * 0.2f);
-                    }
-
-                    freeCardSlots[i]=false;
-                    deck.Remove(RandomCard);
-                    return;
-                }
-            }
-        }
-    }
-
+    public List<GameObject> initialCards = new List<GameObject>();
+    private List<GameObject> drawnCards = new List<GameObject>();
+    public GameObject canvas;
+    int draggedCardIndex;
     void Start()
     {
-        for(int i=0;i<freeCardSlots.Length;i++)
-        DrawCard();
+        DrawCards(initialCards, true);
+        
+        //sub to events.
+        foreach (GameObject go in drawnCards)
+        {
+            Card card = go.GetComponent<Card>();
+            card.OnCardMovedFromHand += HandleCardMovedFromHand;
+            card.OnCardMovedToHand += HandleCardMovedToHand;
+        }
     }
 
     void Update()
     {
         MovesLeftText.GetComponent<TextMeshProUGUI>().text="Moves left: "+ PlayerPrefs.GetInt("MovesLeft").ToString();
-        
+    }
 
+    private void HandleCardMovedFromHand(Card card)
+    {
+        draggedCardIndex = drawnCards.IndexOf(card.gameObject);
+        drawnCards.Remove(card.gameObject);
+        DrawCards(drawnCards, false);
+    }
+
+    private void HandleCardMovedToHand(Card card)
+    {
+        drawnCards.Insert(draggedCardIndex, card.gameObject);
+        DrawCards(drawnCards, false);
+    }
+    
+    private void DrawCards(List<GameObject> cardObjects, bool toInstantiate)
+    {
+        //should set gap dynamically based on the number of cards.
+        float gap = -100f;
+        float cardWidth = cardObjects[0].GetComponent<RectTransform>().sizeDelta.x;
+
+        float totalWidth = (cardObjects.Count - 1) * (cardWidth + gap);
+        float initialX = -totalWidth / 2f;
+
+        int midpoint = cardObjects.Count / 2;
+
+        for (int i = 0; i < cardObjects.Count; i++)
+        {
+            int rotation = 0;
+
+            if (cardObjects.Count % 2 == 0)
+            {
+                if (i >= midpoint)
+                {
+                    rotation = (midpoint - i - 1) * 5 - 10;
+                }
+                else rotation = (midpoint - i) * 5 + 10;
+            }
+            else
+            {
+                if (i < midpoint)
+                {
+                    rotation = (midpoint - i) * 5 + 10;
+                }
+                else if (i > midpoint)
+                {
+                    rotation = (midpoint - i) * 5 - 10;
+                }
+            }
+
+            //cards get lower the closer to edges they are.
+            int posY = -250;
+
+            if (cardObjects.Count % 2 == 0 && i >= midpoint)
+            {
+                posY -= Math.Abs(midpoint - i - 1) * 20;
+            }
+            else
+            {
+                posY -= Math.Abs(midpoint - i) * 20;
+            }
+
+            float posX = initialX + i * (cardWidth + gap);
+            Vector3 coords = new Vector3(posX, posY, 0);
+
+            if (toInstantiate)
+            {
+                GameObject newCard = Instantiate(cardObjects[i], coords, Quaternion.identity);
+                newCard.transform.SetParent(canvas.transform.Find("Cards"), false);
+                
+                //resize, nes per dideles tos korteles.
+                newCard.transform.localScale = new Vector3(0.7f, 0.7f, 1);
+                newCard.transform.localRotation = Quaternion.Euler(0, 0, rotation);
+                drawnCards.Add(newCard);
+            }
+            else
+            {
+                cardObjects[i].transform.localPosition = coords;
+                cardObjects[i].transform.localRotation = Quaternion.Euler(0, 0, rotation);
+            }
+        }
     }
 }
