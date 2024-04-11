f3a55b706848f992319b2fa55813fa16c0a5495f
JonasKucinskas <jonaskucinskas2@gmail.com>
2024-04-11 23:57:43 +0300

    card camera fix

---
+++ Assets/Scripts/CardSelectionHandler.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardSelectionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+{
+    [SerializeField] private float _verticalMoveAmmount = 30f;
+    [SerializeField] private float _moveTime = 0.1f;
+    [Range(0f, 2f), SerializeField] private float _scaleAmmount = 1.1f;
+
+    private Vector3 _startScale;
+    private Card card;
+    private CardManager cardManager;
+    private Vector2 initialCursorPosition;
+    private void Start()
+    {
+        card = gameObject.GetComponent<Card>();
+        _startScale = transform.localScale;
+        cardManager = GameObject.Find("CardManager").GetComponent<CardManager>();
+    }
+
+    private IEnumerator MoveCard(bool startingAnimation)
+    {
+        Vector3 endPosition;
+        Vector3 endScale;
+        Quaternion endRotation;
+
+        Vector3 originalPosition = gameObject.GetComponent<Card>().originalPosition;
+        Vector3 currentPosition = gameObject.GetComponent<RectTransform>().anchoredPosition3D;
+        Quaternion originalRotation = gameObject.GetComponent<Card>().originalRotation;
+
+        float elapsedTime = 0f;
+        while(elapsedTime < _moveTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if(startingAnimation)
+            {
+                endPosition = originalPosition + new Vector3(0f, _verticalMoveAmmount, 2f);
+                endScale = _startScale * _scaleAmmount;
+                endRotation = Quaternion.Euler(Vector3.zero);
+            }
+            else
+            {
+                endPosition = originalPosition;
+                endScale = _startScale;
+                endRotation = originalRotation;
+            }
+
+
+            Vector3 lerpedPos = Vector3.Lerp(currentPosition, endPosition, elapsedTime / _moveTime);
+            Vector3 lerpedScale = Vector3.Lerp(transform.localScale, endScale, elapsedTime / _moveTime);
+            Quaternion lerpedRotation = Quaternion.Lerp(transform.rotation, endRotation, elapsedTime / _moveTime);
+            
+
+            gameObject.GetComponent<RectTransform>().anchoredPosition3D = lerpedPos;
+            transform.localScale = lerpedScale;
+            gameObject.GetComponent<RectTransform>().localRotation = lerpedRotation;
+            
+            yield return null;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        initialCursorPosition = eventData.position;
+        if (!cardManager.IsDragging())
+        {
+            eventData.selectedObject = gameObject;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (initialCursorPosition == eventData.position)
+        {
+            return;
+        }
+
+        //player is dragging some other card.
+        if (cardManager.IsDragging() && !card.IsDragging)
+        {
+            return;
+        }
+        eventData.selectedObject = null;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        StartCoroutine(MoveCard(true));
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if (!cardManager.IsDragging() && !card.IsDragging)
+        {
+            StartCoroutine(MoveCard(false));
+        }
+    }
+}
