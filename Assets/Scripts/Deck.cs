86b845390d302b048249d11b3c07424428577b30
JonasKucinskas <jonaskucinskas2@gmail.com>
2024-04-17 21:50:58 +0300

    fixed floating deck

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -9,7 +9,7 @@ public class Deck : MonoBehaviour
 
     void Start()
     {
-        boardManager=GameObject.Find("Board").GetComponent<BoardScript>();
+        boardManager = GameObject.Find("Board").GetComponent<BoardScript>();
         UpdateHeight();
     }
 
@@ -19,16 +19,17 @@ public class Deck : MonoBehaviour
 
     private void UpdateHeight()
     {
-        float totalHeight = cards.Count * 0.1f; // Assuming cardHeight is the height of one card
+        float totalHeight = cards.Count * 0.1f; 
 
         if (cards.Count == 0)
         {
             //gameObject.SetActive(false);
             return;
         }
-
+        //transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
         transform.localScale = new Vector3(transform.localScale.x, totalHeight, transform.localScale.z);
-        transform.localPosition = new Vector3(transform.localPosition.x, totalHeight / 2f, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 0.1f, transform.localPosition.z);
+
     }
 
     public GameObject Pop()
